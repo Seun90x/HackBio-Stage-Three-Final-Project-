5725d6494743ec9eeab79b5670b78891bc3cca4a
Seun Akinsulire <93671840+Seun90x@users.noreply.github.com>
2022-07-06 21:36:09 +0100

    Add files via upload

---
+++ raw data/download.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+curl -L ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR197/022/SRR19756222/SRR19756222_1.fastq.gz -o SRR19756222_1.fastq.gz
+
+curl -L ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR197/022/SRR19756222/SRR19756222_2.fastq.gz -o SRR19756222_2.fastq.gz
+
+curl -L ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR197/021/SRR19756221/SRR19756221_1.fastq.gz -o SRR19756221_1.fastq.gz
+
+curl -L ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR197/021/SRR19756221/SRR19756221_2.fastq.gz -o SRR19756221_2.fastq.gz
